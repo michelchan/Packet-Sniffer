22f3c5627d9adabfcaa57a90e0c28ebcf0e6977e
Shanshan- <shanon5656@gmail.com>
2016-10-24 19:27:54 -0400

    cleaned up formatting of output

--- dnsParser.py
+++ dnsParser.py
@@ -9,22 +9,33 @@ def convertRaw(x):
     elif x[3].ancount > 0:
         x[2+offset].decode_payload_as(scapy.layers.dns.DNSRR)
         offset += 1
-    elif x[3].nscount > 0:
-        x[2+offset].decode_payload_as(scapy.layers.dns.DNSQR)
-        offset += 1
-    elif x[3].arcount > 0:
-        x[2+offset].decode_payload_as(scapy.layers.dns.DNSQR)
-        offset += 1
 
+def getType(value):
+    if int(value) == 1:
+        return "A"
+    if int(value) == 6:
+        return "SOA"
+    if int(value) == 28:
+        return "AAAA"
+    if int(value) == 5:
+        return "CNAME"
+    if int(value) == 15:
+        return "MX"
+    if int(value) == 2:
+        return "NS"
+    if int(value) == 12:
+        return "PTR"
+    if int(value) == 37:
+        return "CERT"
 
+def dnsParser(packets, filename):
+    print "Hello world"
 
 sys.stdout = open("DNSdump.txt", "w")
-
 count = 20
 packets = sniff(filter="udp", timeout = 10)
-packets.nsummary()
-
-print "\n==========================\nStart parsing\n"
+# packets.nsummary()
+# print "\n==========================\n"
 
 i = 0
 for x in packets:
@@ -35,18 +46,31 @@ for x in packets:
         continue
 
     # ensure that the contents are not read as raw
-    print "Packet " + str(i)
     if type(x[3]) == scapy.packet.Raw:
         try:
             convertRaw(x)
         except:
-            print "Error occured in converting packet " + str(i)
+            sys.stderr.write("Error occured in converting packet %d\n" % i)
             i += 1
             continue
 
-    # print packet
+    """# print packet
     print x[3].id
-    print x.show()
+    print x.show()"""
+
+    # print request / response
+    print "Packet " + str(i)
+    if int(x[3].qdcount) > 0:  # there is a query record
+        qType = getType(x[4].qtype)
+        qName = str(x[4].qname)
+        print "DNS query of type " + qType + " for " + qName
+    if int(x[3].ancount) > 0:  # there is an answer record
+        qType = getType(x[5].type)
+        qName = str(x[5].rrname)
+        qResult = str(x[5].rdata)
+        print "DNS response of type " + qType + " for " + qName + " returned " + qResult
+
+    # advance
     i += 1
     print "\n=================================\n"
 
