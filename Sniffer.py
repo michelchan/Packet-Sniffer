7030ed78c5c4ac938e5e1a48458ee7859dc713d8
Shanshan- <shanon5656@gmail.com>
2016-10-23 21:21:03 -0400

    added debugs to the sniffer file

--- Sniffer.py
+++ Sniffer.py
@@ -6,20 +6,20 @@ timeout = 5
 # packets = sniff(count=numPackets)
 packets = sniff(timeout=timeout)
 
-sys.stdout = open("dump.txt", "a")
-for x in range(0, numPackets - 1):
-    #extract relevant info
+sys.stdout = open("dump.txt", "w")
+for x in range(0, len(packets)):
+    # extract relevant info
     proto = packets[x][1].proto
     srcport = packets[x][2].sport
     dstport = packets[x][2].dport
-    print "Packet# \n" + str(x)
+    print "Packet # " + str(x)
 
     # determine packet type
     if srcport == 80 or dstport == 80:
         print "This is a HTTP packet: \n"
     elif srcport == 443 or dstport == 443:
         print "This is a HTTPS packet: \n"
-    elif srcport == 40 or dstport == 40:
+    elif srcport == 53 or dstport == 53:
         print "This is a DNS packet: \n"
     else:
         print "This is not a recognizable packet: \n"
