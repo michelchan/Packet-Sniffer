f3741af34504550b354a5f4d9f2113826c83c613
Michelle <michelchan421@gmail.com>
2016-10-24 19:50:35 -0400

    Fixed search, now it outputs to file. Fixed how main takes inputs

--- search.py
+++ search.py
@@ -1,16 +1,37 @@
 import re
 
-searchString = ''
-file = ''
-with open(file, 'r') as f:
-    dump = f.readlines()
 
-for line in dump:
-    if searchString in line:
-        print line
+def findKeyword(keyword):
+    file = 'dump.txt'
 
-s = re.split(searchString, dump)
-array = []
-for i in dump:
-    if re.search(searchString, i):
-          array.append(i)
+    with open(file, 'r') as f:
+        dump = f.readlines()
+
+    packets = []
+    packet = ""
+    found = False
+    for line in dump:
+        # if re.search(keyword, line):
+        #     withNewLine = line + "\n"
+        #     packet += withNewLine
+        #     print withNewLine
+        # if re.match("=================================", line):
+        #     packets.append(packet)
+        #     packet = ""
+
+        if re.match("=================================", line):
+            if found:
+                packets.append(packet)
+            packet = ''
+            found = False
+        else:
+            if re.search(keyword, line):
+                found = True
+            withNewLine = line + "\n"
+            packet += withNewLine
+
+    output = open("searchresults.txt", "w")
+    for i in packets:
+        output.write("%s\n" % i)
+
+    print "All packets containing " + keyword + " are available in 'searchresults.txt'"
