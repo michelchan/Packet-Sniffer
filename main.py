614732609e6ff82c5136b28932274f4acdf0f535
Michelle <michelchan421@gmail.com>
2016-10-24 17:49:58 -0400

    Made a simple arg parser

--- main.py
+++ main.py
@@ -1,4 +1,35 @@
-import sys
+def sniff(time):
+    print "sniff"
 
-arguments = sys.argv
+def parse():
+    print "parse"
 
+def search(keyword):
+    print "search"
+
+def html():
+    print "html"
+
+def dns():
+    print "dns"
+
+import sys, getopt
+
+opts, args = getopt.getopt(sys.argv[1:], 'o:v',["sniff=",
+                                            "parse",
+                                            "search=",
+                                            "html",
+                                            "dns",
+                                         ])
+
+for opt, arg in opts:
+    if opt == "--sniff":
+        sniff(arg)
+    elif opt == "--parse":
+        parse()
+    elif opt == "--search":
+        search(arg)
+    elif opt == "--html":
+        html()
+    elif opt == "--dns":
+        dns()
