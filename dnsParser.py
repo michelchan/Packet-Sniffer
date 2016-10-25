d251df71915f6480ac8b665b7881786578ad90bd
Shanshan- <shanon5656@gmail.com>
2016-10-24 23:25:16 -0400

    dns debugging complete

--- dnsParser.py
+++ dnsParser.py
@@ -62,10 +62,11 @@ def dnsParser(packets, filename):
         if numAns > 0:  # there is an answer record
             a = 1
             while a <= numAns:
-                qType = getType(x[4+x].type)
-                qName = str(x[4+x].rrname)
-                qResult = str(x[4+x].rdata)
+                qType = getType(x[4 + a].type)
+                qName = str(x[4 + a].rrname)
+                qResult = str(x[4 + a].rdata)
                 print "DNS response of type " + qType + " for " + qName + " returned " + qResult
+                a += 1
 
         # advance
         i += 1
@@ -74,7 +75,7 @@ def dnsParser(packets, filename):
     sys.stdout = sys.__stdout__
 
 
-# """ Debugging lines
+""" Debugging lines
 sys.stdout = open("DNSdump.txt", "w")
 count = 20
 # packets = sniff(filter="udp", timeout=5)
@@ -82,47 +83,4 @@ count = 20
 packets = rdpcap("packets.cap")
 # wrpcap("programPackets.cap", packets)
 packets.show()
-print "\n==========================\n"
-i = 0
-for x in packets:
-
-    # skip this packet if it is not an DNS packet
-    if x[2].sport != 53 and x[2].dport != 53:
-        i += 1
-        continue
-
-    # ensure that the contents are not read as raw
-    if type(x[3]) == scapy.packet.Raw:
-        try:
-            convertRaw(x)
-        except:
-            sys.stderr.write("Error occured in converting packet %d\n" % i)
-            i += 1
-            continue
-
-    """# print packet
-    print x[3].id
-    print x.show()"""
-
-    # print request / response
-    print "Packet " + str(i)
-    numQueries = int(x[3].qdcount)
-    numAns = int(x[3].ancount)
-    if numQueries > 0:  # there is a query record
-        qType = getType(x[4].qtype)
-        qName = str(x[4].qname)
-        print "DNS query of type " + qType + " for " + qName
-    if numAns > 0:  # there is an answer record
-        a = 1
-        while a <= numAns:
-            qType = getType(x[4+a].type)
-            qName = str(x[4+a].rrname)
-            qResult = str(x[4+a].rdata)
-            print "DNS response of type " + qType + " for " + qName + " returned " + qResult
-            a += 1
-
-    # advance
-    i += 1
-    print "\n=================================\n"
-
-sys.stdout = sys.__stdout__
+print "\n==========================\n"""
